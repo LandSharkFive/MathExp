3b38c4e367d7ee2e29b9674777377d9dca7c6c2a
Bruce Koivu <153857026+LandSharkFive@users.noreply.github.com>
2024-03-05 23:13:42 -0500

    Update Program.cs
    Add filename

--- Program.cs
+++ Program.cs
@@ -5,6 +5,48 @@
 
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Shell();
+                return;
+            }
+
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string option = args[i].ToUpper().Trim();
+                    if (option == "-H")
+                    {
+                        Usage();
+                        return;
+                    }
+                }
+            }
+
+            if (args.Length == 1)
+            {
+                string filename = args[0];
+                ReadFile(filename);
+                return;
+            }
+
+            if (args.Length > 2)
+            {
+                Usage();
+            }
+
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage: RpnOne [filename] ");
+            Console.WriteLine("FileName optional.");
+            Console.WriteLine("Purpose:  Command-Line Calculator.");
+        }
+
+        static void Shell()
+        {
             Util util = new Util();
             double result = 0.0;
 
@@ -14,12 +56,11 @@
                 var input = Console.ReadLine();
                 if (String.IsNullOrEmpty(input))
                 {
-                    break; 
+                    break;
                 }
 
                 if (Console.IsInputRedirected)
                 {
-                    // Echo 
                     Console.WriteLine(input);
                 }
 
@@ -28,9 +69,48 @@
                     result = util.Eval(input);
                     Console.WriteLine(result);
                 }
-                catch (Exception ex) 
-                { 
+                catch (Exception ex)
+                {
                     Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+        static void ReadFile(string fileName)
+        {
+            Util util = new Util();
+            double result = 0.0;
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("FileName not found:  {0}", fileName);
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (true)
+                {
+                    Console.Write("> ");
+                    var input = sr.ReadLine();
+                    if (String.IsNullOrEmpty(input))
+                    {
+                        break;
+                    }
+
+                    // Echo 
+                    Console.WriteLine(input);
+
+                    try
+                    {
+                        result = util.Eval(input);
+                        Console.WriteLine(result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
                 }
             }
         }
